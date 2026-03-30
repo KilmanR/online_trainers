48e21fca029d131b7f6a92b4173fd64137bd8336
AlexR <avrusin1978@gmail.com>
2026-03-30 16:34:28 +0500

    задание про шахматы на степик

--- stepik.py
+++ stepik.py
@@ -369,3 +369,83 @@
 #         print(a1)
 #     else:
 #         print(a1, b2)
+
+# year = int(input())
+# if year % 100 == 0:
+#     print('YES')
+# else:
+#     print('NO')
+
+# a1,b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+# if (a1 + b1 + a2 + b2) % 2 == 0:
+#     print('YES')
+# else:
+#     print('NO')
+
+# age = int(input())
+# g = input()
+# if g == 'm':
+#     print('NO')
+# if g == 'f':
+#     if 10 <= age <= 15:
+#         print('YES')
+#     else:
+#         print('NO')
+
+# num = int(input())
+# if num == 1:
+#     print('I')
+# elif num == 2:
+#     print('II')
+# elif num == 3:
+#     print('III')
+# elif num == 4:
+#     print('IV')
+# elif num == 5:
+#     print('V')
+# elif num == 6:
+#     print('VI')
+# elif num == 7:
+#     print('VII')
+# elif num == 8:
+#     print('VIII')
+# elif num == 9:
+#     print('IX')
+# elif num == 10:
+#     print('X')
+# else:
+#     print('ошибка')
+
+# num = int(input())
+# if num % 2 != 0:
+#     print('YES')
+# elif (2 <= num <= 5) and num % 2 == 0:
+#     print('NO')
+# elif (6 <= num <= 20) and num % 2 == 0:
+#     print('YES')
+# elif num > 20 and num % 2 == 0:
+#     print('NO')
+
+# Если разность x1 и y1 равны разности x2 и y2. Или если суммы равны ,значит да. Иначе нет
+
+# x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+# if (x1 - y1 == x2 - y2) or (x1 + y1 == x2 + y2):
+#     print('YES')
+# else:
+#     print('NO')
+
+# x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+# if (x1 - x2 == 1 or x2 - x1 == 1) and (y1 - y2 == 2 or y2 - y1 == 2):
+#     print('YES')
+# elif (x1 - x2 == 2 or x2 - x1 == 2) and (y1 - y2 == 1 or y2 - y1 == 1):
+#     print('YES')
+# else:
+#     print('NO')
+
+x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+if (x1 == x2 and y1 != y2) or (x1 != x2 and y1 == y2):
+    print('YES')
+elif ((x1 - x2) ** 2) == ((y1 - y2) ** 2):
+    print('YES')
+else:
+    print('NO')
